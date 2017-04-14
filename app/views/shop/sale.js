9904ec6fff8e6f6d7c46f5249dc92a17a5c2d203
Ayabonga <aya@westerncapelabs.com>
2017-04-14 22:25:00 +0200

    Defined shopping view

---
+++ app/views/shop/sale.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class Sale extends Component {
+  render(){
+    return (
+      <div>
+        <h2>Sale</h2>
+        <div>Some contact page content</div>
+      </div>
+    )
+  }
+}
+
+export default Sale;
