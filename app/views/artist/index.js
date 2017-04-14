69dd2e48a5fcc26a5add6e6a6f58956c7a0b67c8
Ayabonga <aya@westerncapelabs.com>
2017-04-14 22:22:40 +0200

    Defined Artis view

---
+++ app/views/artist/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class Artists extends Component {
+  render(){
+    return (
+      <div>
+        <h2>Artists</h2>
+        <div>Some contact page content</div>
+      </div>
+    )
+  }
+}
+
+export default Artists;
