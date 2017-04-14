96b2b871169ae6e5e492d73b7dcc67393b8d82ba
Ayabonga <aya@westerncapelabs.com>
2017-04-14 18:23:39 +0200

    Initialized app views

---
+++ views/NoMatch.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class NoMatch extends Component {
+  render(){
+    return (
+      <div>
+        <h2>NoMatch</h2>
+        <div>404 error</div>
+      </div>
+    )
+  }
+}
+export default NoMatch;
---
+++ views/work.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Work extends Component {
+  render(){
+    return (
+      <div>
+        <h2>Work</h2>
+        <div>Some work page content</div>
+      </div>
+    )
+  }
+}
+export default Work;
