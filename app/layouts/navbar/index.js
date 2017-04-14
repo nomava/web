dc5ccaf0fd439e37d71fe656b89ab5cb0ee7084b
Ayabonga <aya@westerncapelabs.com>
2017-04-14 22:20:37 +0200

    Swapped from using sidebar to Menu inside grid column

--- app/layouts/navbar/index.js
+++ app/layouts/navbar/index.js
@@ -1,41 +1,132 @@
 import React, { Component } from 'react'
-import { Sidebar, Segment, Button, Menu, Image, Icon, Header } from 'semantic-ui-react'
+import { Grid, Sidebar, Segment, Button, Menu, Image, Icon, Header } from 'semantic-ui-react'
 import { Link } from 'react-router'
 
 export class Navbar extends Component {
   render() {
     let visible =true
     return (
-      <div style={{height:'100vh'}}>
+      <div >
+        <Grid>
+          <Grid.Column width={3}>
+            <Menu fixed inverted vertical style={{height:'100vh'}}>
+              <Menu.Item>
+                <Menu.Header>Music</Menu.Header>
 
-        <Sidebar.Pushable as={Segment}>
-          <Sidebar as={Menu} animation='push' width='thin' visible={visible} icon='labeled' vertical inverted>
-            <Menu.Item name='home'>
-              <Link to="/">
-                <Icon name='home' />
-                Home
-              </Link>
-            </Menu.Item>
-            <Menu.Item name='gamepad'>
-              <Icon name='gamepad' />
-              <Link to="/about">About</Link>
-            </Menu.Item>
-            <Menu.Item name='camera'>
-              <Icon name='camera' />
-              <Link to="/work">Work</Link>
-            </Menu.Item>
-            <Menu.Item name='camera'>
-              <Icon name='camera' />
-              <Link to="/contact">Contact</Link>
-            </Menu.Item>
-          </Sidebar>
-          <Sidebar.Pusher>
-            <Segment basic>
-              <Header as='h3'>Application Content</Header>
-              { this.props.children }
+                <Menu.Menu>
+                  <Menu.Item>
+                    <Link to="/latest">
+                      <Icon name='asterisk' />
+                      Latest
+                    </Link>
+                  </Menu.Item>
+                  <Menu.Item>
+                    <Link to="/trending">
+                      <Icon name='hashtag' />
+                      Trending
+                    </Link>
+                  </Menu.Item>
+                  <Menu.Item>
+                    <Link to="/playlists">
+                      <Icon name='list' />
+                      Playlists
+                    </Link>
+                  </Menu.Item>
+                  <Menu.Item>
+                    <Link to="/find">
+                      <Icon name='search' />
+                      Search
+                    </Link>
+                  </Menu.Item>
+                  <Menu.Item>
+                    <Link to="/genres">
+                      <Icon name='unordered list' />
+                      Genres
+                    </Link>
+                  </Menu.Item>
+                </Menu.Menu>
+              </Menu.Item>
+
+              <Menu.Item>
+                <Menu.Header>Artists</Menu.Header>
+                  <Menu.Menu>
+                    <Menu.Item>
+                      <Link to="/artists/popular">
+                        <Icon name='favorite' />
+                        Popular
+                      </Link>
+                    </Menu.Item>
+                    <Menu.Item>
+                      <Link to="/artists/find">
+                        <Icon name='search' />
+                        Search
+                      </Link>
+                    </Menu.Item>
+                    <Menu.Item>
+                      <Link to="/artists/join">
+                        <Icon name='add user' />
+                        Join
+                      </Link>
+                    </Menu.Item>
+                  </Menu.Menu>
+              </Menu.Item>
+
+              <Menu.Item>
+                <Menu.Header>Shop</Menu.Header>
+                  <Menu.Menu>
+                    <Menu.Item>
+                      <Link to="/shop/sale">
+                        <Icon name='plus cart' />
+                        On Sale
+                      </Link>
+                    </Menu.Item>
+                    <Menu.Item>
+                      <Link to="/shop/hot">
+                        <Icon name='line graph' />
+                        Hottest
+                      </Link>
+                    </Menu.Item>
+                    <Menu.Item>
+                      <Link to="/shop/browse">
+                        <Icon name='numbered list' />
+                        Browse
+                      </Link>
+                    </Menu.Item>
+                  </Menu.Menu>
+              </Menu.Item>
+
+              <Menu.Item>
+                <Menu.Header>Support</Menu.Header>
+
+                  <Menu.Menu>
+                    <Menu.Item>
+                      <Link to="/support/about">
+                        <Icon name='search' />
+                        About
+                      </Link>
+                    </Menu.Item>
+                    <Menu.Item>
+                      <Link to="/support/contact">
+                        <Icon name='facebook square' />
+                        Contact
+                      </Link>
+                    </Menu.Item>
+                    <Menu.Item>
+                      <Link to="/support/faqs">
+                        <Icon name='question circle' />
+                        How Stuff works
+                      </Link>
+                    </Menu.Item>
+                  </Menu.Menu>
+                </Menu.Item>
+            </Menu>
+          </Grid.Column>
+          <Grid.Column stretched width={13}>
+            <Segment>
+              {this.props.children}
             </Segment>
-          </Sidebar.Pusher>
-        </Sidebar.Pushable>
+          </Grid.Column>
+        </Grid>
       </div>
     )
   }
