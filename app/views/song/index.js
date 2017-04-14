e34307883f1be98e3305342c88fabaf971dda2f1
Ayabonga <aya@westerncapelabs.com>
2017-04-14 22:25:25 +0200

    Defined song view

---
+++ app/views/song/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class Contact extends Component {
+  render(){
+    return (
+      <div>
+        <h2>Contact</h2>
+        <div>Some contact page content</div>
+      </div>
+    )
+  }
+}
+
+export default Contact;
