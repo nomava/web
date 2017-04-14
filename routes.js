c7a4572d1d66d8cf4d20998e29063802af69a2f4
Ayabonga <aya@westerncapelabs.com>
2017-04-14 18:59:32 +0200

    Removed unnecessary work route

--- routes.js
+++ routes.js
@@ -1,19 +1,15 @@
 // routes.js
 import React, { Component } from 'react'
 import { Route, IndexRoute, Link } from 'react-router'
-import App from './views/app'
-import Home from './views/home'
-import About from './views/about'
-import Contact from './views/contact'
-import Work from './views/work'
-import NoMatch from './views/NoMatch'
+import App from './app'
+import { Home, About, Contact, NoMatch } from './app/views'
+
 
 
 export default (
   <Route path="/" component={App}>
     <IndexRoute component={Home}/>
     <Route path="about" component={About}/>
-    <Route path="work" component={Work}/>
     <Route path="contact" component={Contact}/>
     <Route path="*" component={NoMatch}/>
   </Route>
