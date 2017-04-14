324d0b45d840cfdd5743c98474714894397809b7
Ayabonga <aya@westerncapelabs.com>
2017-04-15 01:34:34 +0200

    Modified Nav Icons

--- app/layouts/navbar/index.js
+++ app/layouts/navbar/index.js
@@ -3,20 +3,34 @@ import { Grid, Sidebar, Segment, Button, Menu, Image, Icon, Header } from 'seman
 import { Link } from 'react-router'
 
 export class Navbar extends Component {
+  constructor(props) {
+    super(props);
+
+    // Set up initial state
+    this.state = { activeItem: 'gamepad' }
+
+    // Functions must be bound manually with ES6 classes
+    this.handleItemClick = this.handleItemClick.bind(this);
+  }
+
+
+  handleItemClick (e, { name }){
+    this.setState({ activeItem: name })
+  }
   render() {
-    let visible =true
+    const { activeItem } = this.state
     return (
       <div >
         <Grid>
-          <Grid.Column width={3}>
-            <Menu fixed="left" inverted vertical style={{height:'100vh'}}>
+          <Grid.Column width={4}>
+            <Menu fixed="left" inverted vertical style={{width:'18rem'}}>
               <Menu.Item>
                 <Menu.Header>Music</Menu.Header>
 
                 <Menu.Menu>
                   <Menu.Item>
                     <Link to="/latest">
-                      <Icon name='asterisk' />
+                      <Icon name='wait' />
                       Latest
                     </Link>
                   </Menu.Item>
@@ -28,7 +42,7 @@ export class Navbar extends Component {
                   </Menu.Item>
                   <Menu.Item>
                     <Link to="/playlists">
-                      <Icon name='list' />
+                      <Icon name='unordered list' />
                       Playlists
                     </Link>
                   </Menu.Item>
@@ -40,37 +54,33 @@ export class Navbar extends Component {
                   </Menu.Item>
                   <Menu.Item>
                     <Link to="/genres">
-                      <Icon name='unordered list' />
+                      <Icon name='group' />
                       Genres
                     </Link>
                   </Menu.Item>
                 </Menu.Menu>
               </Menu.Item>
 
-              <Menu.Item>
-                <Menu.Header>Artists</Menu.Header>
-                  <Menu.Menu>
-                    <Menu.Item>
-                      <Link to="/artists/popular">
-                        <Icon name='favorite' />
-                        Popular
-                      </Link>
-                    </Menu.Item>
-                    <Menu.Item>
-                      <Link to="/artists/find">
-                        <Icon name='search' />
-                        Search
-                      </Link>
-                    </Menu.Item>
-                    <Menu.Item>
-                      <Link to="/artists/join">
-                        <Icon name='add user' />
-                        Join
-                      </Link>
-                    </Menu.Item>
-                  </Menu.Menu>
-              </Menu.Item>
 
+
+              <Menu.Item>
+                 <Segment>
+                   <Image src='https://www.bellanaija.com/wp-content/uploads/2015/08/Album-Front1.jpg' size='medium' centered bordered />
+                 </Segment>
+                <Menu icon='labeled' inverted>
+                  <Menu.Item name='gamepad' active={activeItem === 'gamepad'} onClick={this.handleItemClick}>
+                    <Icon name='backward' />
+                  </Menu.Item>
+
+                  <Menu.Item name='video camera' active={activeItem === 'video camera'} onClick={this.handleItemClick}>
+                    <Icon name='play' />
+                  </Menu.Item>
+
+                  <Menu.Item name='video play' active={activeItem === 'video play'} onClick={this.handleItemClick}>
+                    <Icon name='forward' />
+                  </Menu.Item>
+                </Menu>
+              </Menu.Item>
               <Menu.Item>
                 <Menu.Header>Shop</Menu.Header>
                   <Menu.Menu>
@@ -86,7 +96,7 @@ export class Navbar extends Component {
                         Hottest
                       </Link>
                     </Menu.Item>
-                    <Menu.Item>
+                    <Menu.Item >
                       <Link to="/shop/browse">
                         <Icon name='numbered list' />
                         Browse
@@ -94,34 +104,15 @@ export class Navbar extends Component {
                     </Menu.Item>
                   </Menu.Menu>
               </Menu.Item>
-
               <Menu.Item>
-                <Menu.Header>Support</Menu.Header>
-
-                  <Menu.Menu>
-                    <Menu.Item>
-                      <Link to="/support/about">
-                        <Icon name='search' />
-                        About
-                      </Link>
-                    </Menu.Item>
-                    <Menu.Item>
-                      <Link to="/support/contact">
-                        <Icon name='facebook square' />
-                        Contact
-                      </Link>
-                    </Menu.Item>
-                    <Menu.Item>
-                      <Link to="/support/faqs">
-                        <Icon name='question circle' />
-                        How Stuff works
-                      </Link>
-                    </Menu.Item>
-                  </Menu.Menu>
-                </Menu.Item>
+                <Link to="/support/about">
+                  <Icon name='question circle' />
+                  Support
+                </Link>
+              </Menu.Item>
             </Menu>
           </Grid.Column>
-          <Grid.Column stretched width={13}>
+          <Grid.Column stretched width={11}>
             <Segment>
               {this.props.children}
             </Segment>
