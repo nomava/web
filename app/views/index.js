72ebb2277026ddcff6048d4d60dfcb8fa7a95932
Ayabonga <aya@westerncapelabs.com>
2017-04-14 18:58:53 +0200

    Restructured App views

---
+++ app/views/index.js
@@ -0,0 +1,10 @@
+
+import Home from './home'
+import About  from './about'
+import NoMatch from './NoMatch'
+import Contact from './contact'
+
+export default Home;
+export default About;
+export default NoMatch;
+export default Contact;
