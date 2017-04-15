7d973371a5e821770121b2f705f6902d83ab431b
Ayabonga <aya@westerncapelabs.com>
2017-04-15 21:45:07 +0200

    Added trending skeleton view

--- app/views/trending/index.js
+++ app/views/trending/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Icon, Header } from 'semantic-ui-react'
+import { Button, Grid, Image, Icon, Header } from 'semantic-ui-react'
 
 class Trending extends Component {
   render(){
@@ -11,7 +11,77 @@ class Trending extends Component {
             Trending
           </Header.Content>
         </Header>
+        <Button.Group labeled>
+          <Button icon='play' content='Play' />
+          <Button icon='shuffle' content='Shuffle' />
+        </Button.Group>
+        <Grid celled>
+          <Grid.Row>
+            <Grid.Column width={3}>
+              <Image src='/assets/img/olothando-1.jpg' />
+            </Grid.Column>
+            <Grid.Column width={10}>
+              <h3> Ndofika nini </h3>
+              <p>By Olothando Ndamase</p>
+            </Grid.Column>
+            <Grid.Column width={3}>
+              <div className='ui two buttons'>
+                <Button color='black'>
+                  <Icon name='play' />
+                  Listen
+                </Button>
+                <Button color='yellow'>
+                  <Icon name='cart' />
+                  Buy
+                </Button>
+              </div>
+            </Grid.Column>
+          </Grid.Row>
 
+          <Grid.Row>
+            <Grid.Column width={3}>
+              <Image src='/assets/img/skit.png' />
+            </Grid.Column>
+            <Grid.Column width={10}>
+                <h3> My bunny boy </h3>
+                <p>By Unchartered Watesr</p>
+            </Grid.Column>
+            <Grid.Column width={3}>
+              <div className='ui two buttons'>
+                <Button color='black'>
+                  <Icon name='play' />
+                  Listen
+                </Button>
+                <Button color='yellow'>
+                  <Icon name='cart' />
+                  Buy
+                </Button>
+              </div>
+            </Grid.Column>
+          </Grid.Row>
+
+          <Grid.Row>
+            <Grid.Column width={3}>
+              <Image src='/assets/img/olwethu-3.jpg' />
+            </Grid.Column>
+            <Grid.Column width={10}>
+                <h3> Noba Yini Dali  </h3>
+                <p>By Rhatala</p>
+            </Grid.Column>
+            <Grid.Column width={3}>
+              <div className='ui two buttons'>
+                <Button color='black'>
+                  <Icon name='play' />
+                  Listen
+                </Button>
+                <Button color='yellow'>
+                  <Icon name='cart' />
+                  Buy
+                </Button>
+              </div>
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
       </div>
     )
   }
